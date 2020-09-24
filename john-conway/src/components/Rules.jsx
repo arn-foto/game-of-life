c4f51820031bb5d6512ff08101d72f2f85da4a07
Anthony N <foto.arn@gmail.com>
2020-09-23 23:37:42 -0700

    + added styling

---
+++ john-conway/src/components/Rules.jsx
@@ -0,0 +1,24 @@
+// import React from "react";
+
+// export default function () {
+//   return (
+//     <div className="rulez">
+//       <h2>
+//         - Any live cell with fewer than two live neighbors dies, as if by
+//         underpopulation. -
+//       </h2>
+//       <h2>
+//         - Any live cell with two or three live neighbors lives on to the next
+//         generation. -
+//       </h2>
+//       <h2>
+//         - Any live cell with more than three live neighbors dies, as if by
+//         overpopulation. -
+//       </h2>
+//       <h2>
+//         - Any dead cell with exactly three live neighbors becomes a live cell,
+//         as if by reproduction. -
+//       </h2>
+//     </div>
+//   );
+// }
